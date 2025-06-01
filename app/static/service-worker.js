7c8e10362740daebe1bdce521054811bc736c893
Tomas <tomas.bajtler@gmail.com>
2025-06-01 21:08:43 +0200

    Přidány PWA soubory a aktualizace HTML pro manifest

---
+++ app/static/service-worker.js
@@ -0,0 +1,9 @@
+self.addEventListener("install", (e) => {
+  console.log("Service Worker installed");
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => new Response("Offline"))
+  );
+});
